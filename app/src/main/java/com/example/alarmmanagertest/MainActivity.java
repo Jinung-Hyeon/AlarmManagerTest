0469a228243fc7f077c9e19a4b3856a38ba1e43c
u <oiuoiyu@naver.com>
2022-06-28 17:27:41 +0900

    매니페스트 오버레이 추가, 메인액티비티 오버레이 권한 창 띄우기,

--- app/src/main/java/com/example/alarmmanagertest/MainActivity.java
+++ app/src/main/java/com/example/alarmmanagertest/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -17,6 +20,8 @@ import android.widget.Toast;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "timetest";
+    private static final int ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE = 1;
     Button start;
 
     @Override
@@ -25,6 +30,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {   // 마시멜로우 이상일 경우
+            if (!Settings.canDrawOverlays(this)) {              // 체크
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                        Uri.parse("package:" + getPackageName()));
+                startActivityForResult(intent, ACTION_MANAGE_OVERLAY_PERMISSION_REQUEST_CODE);
+            }
+        }
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED
                 | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD
                 | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON
@@ -41,13 +53,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void startAlert() {
-        EditText text = findViewById(R.id.time);
-        int i = Integer.parseInt(text.getText().toString());
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, 5);
+        c.set(Calendar.MINUTE, 26);
+        c.set(Calendar.SECOND, 0);
+        Log.d(TAG, "time : " + c.getTime() + " getTimeInMillis : " + c.getTimeInMillis() + " currentTime : " + System.currentTimeMillis());
+
+        //EditText text = findViewById(R.id.time);
+        //int i = Integer.parseInt(text.getText().toString());
         Intent intent = new Intent(this, Alarm.class);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(this.getApplicationContext(), 234324243, intent, 0);
         AlarmManager alarmManager = (AlarmManager)getSystemService(ALARM_SERVICE);
-        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + (i * 1000), pendingIntent);
-        Toast.makeText(this, "Alarm set:: " + i + " seconds", Toast.LENGTH_LONG).show();
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), 24 * 60 * 60 * 1000, pendingIntent);
+        //Toast.makeText(this, "Alarm set:: " + i + " seconds", Toast.LENGTH_LONG).show();
     }
 
 
